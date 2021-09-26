78711be2f5fadb80776c9af1481bc2e677c32062
nitin-dhevar <nitin.dhanabalan@gmail.com>
2021-09-26 16:45:47 +0530

    Add precompiled assets for Heroku

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 from os.path import join, dirname, realpath
 
-
 app = Flask(__name__)
 
 # enable debugging mode
@@ -11,26 +10,6 @@ app.config["DEBUG"] = True
 # Upload folder
 UPLOAD_FOLDER = 'static/files'
 app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
-
-
-# Database
-# mydb = mysql.connector.connect(
-#   host="localhost",
-#   user="root",
-#   password="",
-#   database="csvdata"
-# )
-
-# mycursor = mydb.cursor()
-
-# mycursor.execute("SHOW DATABASES")
-
-# # View All Database
-# for x in mycursor:
-#   print(x)
-
-
-
 
 
 # Root URL
@@ -49,22 +28,8 @@ def uploadFiles():
            file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
           # set the file path
            uploaded_file.save(file_path)
-           #parseCSV(file_path)
           # save the file
       return redirect(url_for('index'))
-
-# def parseCSV(filePath):
-#       # CVS Column Names
-#       col_names = ['first_name','last_name','address', 'street', 'state' , 'zip']
-#       # Use Pandas to parse the CSV file
-#       csvData = pd.read_csv(filePath,names=col_names, header=None)
-#       # Loop through the Rows
-#       for i,row in csvData.iterrows():
-#              sql = "INSERT INTO addresses (first_name, last_name, address, street, state, zip) VALUES (%s, %s, %s, %s, %s, %s)"
-#              value = (row['first_name'],row['last_name'],row['address'],row['street'],row['state'],str(row['zip']))
-#              mycursor.execute(sql, value, if_exists='append')
-#              mydb.commit()
-#              print(i,row['first_name'],row['last_name'],row['address'],row['street'],row['state'],row['zip'])
 
 if (__name__ == "__main__"):
      app.run(port = 5000)
